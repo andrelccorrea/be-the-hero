efa81c5879a97275edca2049c4c9f79b4837daa1
andrelccorrea <andre.lcosta@hotmail.com>
2020-03-26 20:18:55 -0300

    Back-end da aplicacão

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+
+// usado para que o corpo das requisicões seja aceito em Json
+app.use(express.json());
+//  usado para permitir o acesso aos recursos por outros domínios (CORS=CROSS ORIGIN RESOURCE SHARING)
+app.use(cors);
+// usado para que o app utilize a variável routes, que referencia o arquivo ./routes
+app.use(routes);
+// usado para definir a porta de escuta
+app.listen(3333);
